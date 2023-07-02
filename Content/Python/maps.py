e2e930f87d02b6ee4d2075973f467a32d3e1346f
Andrew1407 <endry1407@gmail.com>
2023-07-03 02:15:07 +0300

    map switch implemeted (python scripts)

--- Content/Python/maps.py
+++ Content/Python/maps.py
@@ -13,3 +13,13 @@ def open_form():
 
 def open_playground():
     open(name='ClientsPlayground')
+
+
+def switch():
+    load_destinations = {
+        'ClientSwitchForm': 'ClientsPlayground',
+        'ClientsPlayground': 'ClientSwitchForm',
+    }
+    world = unreal.get_editor_subsystem(unreal.UnrealEditorSubsystem).get_editor_world()
+    name = world.get_name()
+    if name in load_destinations: open(load_destinations[name])
