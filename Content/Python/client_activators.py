f52a201df733fb43eab5c2bb3a7dff81a242f6cb
Andrew1407 <endry1407@gmail.com>
2023-07-03 23:33:09 +0300

    python scripts: constant names defined (maps), asset loader functions added to fill spawned actors with data (activators)

--- Content/Python/client_activators.py
+++ Content/Python/client_activators.py
@@ -5,18 +5,31 @@ import unreal
 OUTLINER_FOLDER = 'ClientStrategies'
 ACTOR_CLASS_NAME = 'BP_StrategyActivator'
 STRATEGY_ACTOR_PATH = '/Game/Core/Blueprints/Gameplay/' + ACTOR_CLASS_NAME
-PROPS_ASSET_PATH = '/Plugins/ClientConnectionStrategies/DataAssets/DA_ClientCollection'
 
-HOST = 'localhost'
-PORT = 8080
+DATA_ASSET_DIR = '/ClientConnectionStrategies/DataAssets/'
+PROPS_ASSET_PATH = DATA_ASSET_DIR + 'DA_DefaultFieldsFilling'
+CLIENT_COLLECTION_ASSET_PATH = DATA_ASSET_DIR + 'DA_ClientCollection'
 
+
+ActorsArr = unreal.Array[unreal.Actor]
+ColorCollection = unreal.Map[unreal.ClientLabels, unreal.Color]
+ActivatorLabels = Optional[Iterable[str]]
+ActivatorsToSearch = Union[str, Iterable[str]]
 
 def get_client_labels() -> dict[str, unreal.ClientLabels]:
     keys = 'NONE', 'HTTP', 'WS', 'UDP', 'TCP'
     return { k: getattr(unreal.ClientLabels, k) for k in keys }
 
 
-def spawn_activators(start: unreal.Vector, step: unreal.Vector, labels: Optional[tuple[str]] = None):
+def load_props_asset() -> unreal.ClientCollection:
+    return unreal.EditorAssetLibrary.load_asset(PROPS_ASSET_PATH)
+
+
+def load_strategies_asset() -> unreal.ClientCollection:
+    return unreal.EditorAssetLibrary.load_asset(CLIENT_COLLECTION_ASSET_PATH)
+
+
+def spawn_activators(start: unreal.Vector, step: unreal.Vector, labels: ActivatorLabels = None) -> ActorsArr:
     client_labels = get_client_labels()
     labels_to_spawn = None
     if labels is None:
@@ -25,7 +38,12 @@ def spawn_activators(start: unreal.Vector, step: unreal.Vector, labels: Optional
         labels_to_spawn = tuple(l.upper() for l in labels if l.upper() in client_labels.keys())
     i = 0
     position_vec = start.copy()
+    props = load_props_asset()
+    styles = load_strategies_asset()
+    client_colors: ColorCollection = styles.get_editor_property('ClientColors')
+    get_color = lambda val: client_colors.get(key=val, default=styles.get_editor_property('DefaultColor'))
     actor_class = unreal.EditorAssetLibrary.load_blueprint_class(STRATEGY_ACTOR_PATH)
+    spawned: ActorsArr = unreal.Array(type=unreal.Actor)
     loading_title = 'Spawning client strategy activators: '
     with unreal.ScopedSlowTask(work=len(labels_to_spawn), desc=loading_title + '...') as slow_task:
         slow_task.make_dialog(can_cancel=True)
@@ -36,22 +54,36 @@ def spawn_activators(start: unreal.Vector, step: unreal.Vector, labels: Optional
             actor = unreal.EditorLevelLibrary().spawn_actor_from_class(actor_class, position_vec)
             actor.set_actor_label(f'{ACTOR_CLASS_NAME}_{label}')
             actor.set_folder_path(OUTLINER_FOLDER)
-            actor.set_editor_property('StrategyType', enum_value)
+            color = get_color(enum_value)
+            actor.set_editor_properties(dict(
+                StrategyType=enum_value,
+                Color=unreal.LinearColor(color.r, color.g, color.b),
+            ))
             position_vec += step
+            spawned.append(actor)
             if enum_value == unreal.ClientLabels.NONE: continue
-            actor.set_editor_properties(dict(Host=HOST, Port=PORT + i))
+            actor.set_editor_properties(dict(
+                Host=props.get_editor_property('Host'),
+                Port=props.get_editor_property('Port') + i,
+            ))
             i += 1
+    return spawned
 
 
-def select_from(search: Union[str, Iterable[str]]):
+def select_from(search: ActivatorsToSearch) -> ActorsArr:
     client_labels = get_client_labels()
     if isinstance(search, str): search = (search, )
     labels = tuple(client_labels[l] for l in set(s.upper() for s in search) if l in client_labels)
     if not labels: return
     world = unreal.get_editor_subsystem(unreal.UnrealEditorSubsystem).get_editor_world()
+    actor_subsystem = unreal.get_editor_subsystem(unreal.EditorActorSubsystem)
     actor_class = unreal.EditorAssetLibrary.load_blueprint_class(STRATEGY_ACTOR_PATH)
     actors = unreal.GameplayStatics.get_all_actors_of_class(world, actor_class)
+    selected: ActorsArr = unreal.Array(type=unreal.Actor)
     for actor in actors:
         strategy_type: unreal.ClientLabels = actor.get_editor_property('StrategyType')
-        if strategy_type in labels:
-            unreal.EditorLevelLibrary.set_actor_selection_state(actor, should_be_selected=True)
+        if strategy_type not in labels: continue
+        actor_subsystem.set_actor_selection_state(actor, should_be_selected=True)
+        selected.append(actor)
+    return selected
+    
--- Content/Python/maps.py
+++ Content/Python/maps.py
@@ -2,23 +2,26 @@ import unreal
 
 
 MAPS_PATH = '/Game/Core/Maps/'
+CLIENT_SWITCH_FORM_MAP = 'ClientSwitchForm'
+CLIENTS_PLAYGROUND_MAP = 'ClientsPlayground'
+
 
 def open(name: str):
     unreal.EditorLoadingAndSavingUtils.load_map(MAPS_PATH + name)
 
 
 def open_form():
-    open(name='ClientSwitchForm')
+    open(name=CLIENT_SWITCH_FORM_MAP)
 
 
 def open_playground():
-    open(name='ClientsPlayground')
+    open(name=CLIENTS_PLAYGROUND_MAP)
 
 
 def switch():
     load_destinations = {
-        'ClientSwitchForm': 'ClientsPlayground',
-        'ClientsPlayground': 'ClientSwitchForm',
+        CLIENT_SWITCH_FORM_MAP: CLIENTS_PLAYGROUND_MAP,
+        CLIENTS_PLAYGROUND_MAP: CLIENT_SWITCH_FORM_MAP,
     }
     world = unreal.get_editor_subsystem(unreal.UnrealEditorSubsystem).get_editor_world()
     name = world.get_name()
