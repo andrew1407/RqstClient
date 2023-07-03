2406cbdfe96d5d99d23af3f801ebefd2346f9b25
Andrew1407 <endry1407@gmail.com>
2023-07-04 00:16:49 +0300

    python scripts: to_default_props function (activators); pyignore added

--- Content/Python/client_activators.py
+++ Content/Python/client_activators.py
@@ -11,10 +11,10 @@ PROPS_ASSET_PATH = DATA_ASSET_DIR + 'DA_DefaultFieldsFilling'
 CLIENT_COLLECTION_ASSET_PATH = DATA_ASSET_DIR + 'DA_ClientCollection'
 
 
-ActorsArr = unreal.Array[unreal.Actor]
+ActorArr = unreal.Array[unreal.Actor]
 ColorCollection = unreal.Map[unreal.ClientLabels, unreal.Color]
 ActivatorLabels = Optional[Iterable[str]]
-ActivatorsToSearch = Union[str, Iterable[str]]
+ActivatorsToHandle = Union[str, Iterable[str]]
 
 def get_client_labels() -> dict[str, unreal.ClientLabels]:
     keys = 'NONE', 'HTTP', 'WS', 'UDP', 'TCP'
@@ -29,7 +29,7 @@ def load_strategies_asset() -> unreal.ClientCollection:
     return unreal.EditorAssetLibrary.load_asset(CLIENT_COLLECTION_ASSET_PATH)
 
 
-def spawn_activators(start: unreal.Vector, step: unreal.Vector, labels: ActivatorLabels = None) -> ActorsArr:
+def spawn_activators(start: unreal.Vector, step: unreal.Vector, labels: ActivatorLabels = None) -> ActorArr:
     client_labels = get_client_labels()
     labels_to_spawn = None
     if labels is None:
@@ -43,7 +43,7 @@ def spawn_activators(start: unreal.Vector, step: unreal.Vector, labels: Activato
     client_colors: ColorCollection = styles.get_editor_property('ClientColors')
     get_color = lambda val: client_colors.get(key=val, default=styles.get_editor_property('DefaultColor'))
     actor_class = unreal.EditorAssetLibrary.load_blueprint_class(STRATEGY_ACTOR_PATH)
-    spawned: ActorsArr = unreal.Array(type=unreal.Actor)
+    spawned: ActorArr = unreal.Array(type=unreal.Actor)
     loading_title = 'Spawning client strategy activators: '
     with unreal.ScopedSlowTask(work=len(labels_to_spawn), desc=loading_title + '...') as slow_task:
         slow_task.make_dialog(can_cancel=True)
@@ -70,16 +70,16 @@ def spawn_activators(start: unreal.Vector, step: unreal.Vector, labels: Activato
     return spawned
 
 
-def select_from(search: ActivatorsToSearch) -> ActorsArr:
+def select_from(search: ActivatorsToHandle) -> ActorArr:
     client_labels = get_client_labels()
-    if isinstance(search, str): search = (search, )
+    if isinstance(search, str): search = (search,)
     labels = tuple(client_labels[l] for l in set(s.upper() for s in search) if l in client_labels)
-    if not labels: return
+    selected: ActorArr = unreal.Array(type=unreal.Actor)
+    if not labels: return selected
     world = unreal.get_editor_subsystem(unreal.UnrealEditorSubsystem).get_editor_world()
     actor_subsystem = unreal.get_editor_subsystem(unreal.EditorActorSubsystem)
     actor_class = unreal.EditorAssetLibrary.load_blueprint_class(STRATEGY_ACTOR_PATH)
     actors = unreal.GameplayStatics.get_all_actors_of_class(world, actor_class)
-    selected: ActorsArr = unreal.Array(type=unreal.Actor)
     for actor in actors:
         strategy_type: unreal.ClientLabels = actor.get_editor_property('StrategyType')
         if strategy_type not in labels: continue
@@ -87,3 +87,26 @@ def select_from(search: ActivatorsToSearch) -> ActorsArr:
         selected.append(actor)
     return selected
     
+
+def to_default_props(targets: Optional[ActivatorsToHandle] = None) -> ActorArr:
+    client_labels = get_client_labels()
+    if targets is None:
+        targets = tuple(client_labels.keys())
+    elif isinstance(targets, str):
+        targets = (targets,)
+    labels = tuple(client_labels[l] for l in set(s.upper() for s in targets) if l in client_labels)
+    if not labels: return unreal.Array(type=unreal.Actor)
+    world = unreal.get_editor_subsystem(unreal.UnrealEditorSubsystem).get_editor_world()
+    actor_class = unreal.EditorAssetLibrary.load_blueprint_class(STRATEGY_ACTOR_PATH)
+    actors = unreal.GameplayStatics.get_all_actors_of_class(world, actor_class)
+    props = load_props_asset()
+    styles = load_strategies_asset()
+    client_colors: ColorCollection = styles.get_editor_property('ClientColors')
+    get_color = lambda val: client_colors.get(key=val, default=styles.get_editor_property('DefaultColor'))
+    for actor in actors:
+        strategy_type: unreal.ClientLabels = actor.get_editor_property('StrategyType')
+        color = get_color(strategy_type)
+        actor.set_editor_property('Color', unreal.LinearColor(color.r, color.g, color.b))
+        if strategy_type != unreal.ClientLabels.NONE:
+            actor.set_editor_property('Host', props.get_editor_property('Host'))
+    return actors
